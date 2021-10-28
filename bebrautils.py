8bda74720d1e7e12eee3c027a874ef8e2ff57541
TRB-Exe <72357112+TRB-Exe@users.noreply.github.com>
2021-10-28 05:29:10 +0000

    Add files via upload

---
+++ bebrautils.py
@@ -0,0 +1,40 @@
+# bebra utils module klass
+import time
+import random
+from random import randint
+
+
+def nuhatbebru():
+    print('НЮХАЙ БЕБРУ!')
+
+def poshelnahui():
+    print('ПОШЕЛ НАХУЙ')
+
+def nuhnibebruloshpedus():
+    print('СЛЫШЬ НЮХНИ БЕБРОЧКИ ЛОШПЕДУС!')
+
+def anonimus():
+    print("снюс анонимус 228 хакер взломает жопу шок ахахахаха ")
+
+def version():
+    print("bebrautils 0.0.1b")
+
+
+def deadinside():
+  S=1000
+  while S!=6:
+    time.sleep(0.1)
+    S=S-7
+    print(S+7,"- 7 =" ,(S))
+    time.sleep(0.1)
+
+def bebrashans():
+  randomlist = ['К сожажелию бебра не найдена', 'http://trbexe.ml/files/bebra.html', 'Бебра найдена но мы ее вам не дадим']
+  print("Bebra Gift")
+  print("Ищем бебру подождите")
+  time.sleep(5.0)
+  print("Шанс получить бебру", randint(0, 5))
+  time.sleep(0.5)
+  print("Это ещё не всё")
+  time.sleep(2.0)
+  print("Ответ: ", random.choice(randomlist))
---
+++ example.py
@@ -0,0 +1,8 @@
+import bebrautils
+import time
+
+bebrautils.nuhnibebruloshpedus()
+bebrautils.poshelnahui()
+bebrautils.deadinside()
+bebrautils.bebrashans()
+bebrautils.version()
